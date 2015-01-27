c137a50b8e042a4d60b0818654f7488060f5acfc
Amir Tocker <tocker.signup@gmail.com>
2015-01-27 15:18:50 +0200

    Fix #16 multiple drag and drop
    * Upgrade ng-file-upload to include fix to the button behaviour
    * Remove shim package - it is included in ng-file-upload
    * Included missing attributes in the angular demo to allow drag and
    drop of multiple files
    * Fixed jQuery sample code to allow multiple drag and drop
    * Compared jQuery UI to the Angular UI to present the details of each
    uploaded file
    * Modified jQuery controller to listen and track file upload progress

--- samples/photo_album/app/js/controllers.js
+++ samples/photo_album/app/js/controllers.js
@@ -7,15 +7,15 @@ var photoAlbumControllers = angular.module('photoAlbumControllers', ['angularFil
 photoAlbumControllers.controller('photoUploadCtrlJQuery', ['$scope', '$rootScope', '$routeParams', '$location',
   /* Uploading with jQuery File Upload */
   function($scope, $rootScope, $routeParams, $location) {
-    
+    $scope.files = {};
     $scope.updateTitle = function(){
       var uploadParams = $scope.widget.fileupload('option', 'formData');
       uploadParams["context"] = "photo=" + $scope.title;
       $scope.widget.fileupload('option', 'formData', uploadParams);
     };
-    
+
     $scope.widget = $(".cloudinary_fileupload")
-      .unsigned_cloudinary_upload($.cloudinary.config().upload_preset, {tags: 'myphotoalbum', context:'photo='}, { 
+      .unsigned_cloudinary_upload($.cloudinary.config().upload_preset, {tags: 'myphotoalbum', context:'photo='}, {
         // Uncomment the following lines to enable client side image resizing and valiation.
         // Make sure cloudinary/processing is included the js file
         //disableImageResize: false,
@@ -23,9 +23,10 @@ photoAlbumControllers.controller('photoUploadCtrlJQuery', ['$scope', '$rootScope
         //imageMaxHeight: 600,
         //acceptFileTypes: /(\.|\/)(gif|jpe?g|png|bmp|ico)$/i,
         //maxFileSize: 20000000, // 20MB
-        dropZone: "#direct_upload",
+        dropZone: "#direct_upload_jquery",
         start: function (e) {
           $scope.status = "Starting upload...";
+          $scope.files = {};
           $scope.$apply();
         },
         fail: function (e, data) {
@@ -33,6 +34,14 @@ photoAlbumControllers.controller('photoUploadCtrlJQuery', ['$scope', '$rootScope
           $scope.$apply();
         }
       })
+      .on("cloudinaryprogress", function (e, data) {
+        var name = data.files[0].name;
+        var file = $scope.files[name] || {};
+        file.progress = Math.round((data.loaded * 100.0) / data.total);
+        file.status = "Uploading... " + file.progress + "%";
+        $scope.files[name] = file;
+        $scope.$apply();
+        })
       .on("cloudinaryprogressall", function (e, data) {
         $scope.progress = Math.round((data.loaded * 100.0) / data.total);
         $scope.status = "Uploading... " + $scope.progress + "%";
@@ -42,10 +51,15 @@ photoAlbumControllers.controller('photoUploadCtrlJQuery', ['$scope', '$rootScope
         $rootScope.photos = $rootScope.photos || [];
         data.result.context = {custom: {photo: $scope.title}};
         $scope.result = data.result;
+        var name = data.files[0].name;
+        var file = $scope.files[name] ||{};
+        file.name = name;
+        file.result = data.result;
+        $scope.files[name] = file;
         $rootScope.photos.push(data.result);
         $scope.$apply();
       });
-  }]).controller('photoUploadCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$upload', 
+  }]).controller('photoUploadCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$upload',
   /* Uploading with Angular File Upload */
   function($scope, $rootScope, $routeParams, $location, $upload) {
     $scope.$watch('files', function() {
