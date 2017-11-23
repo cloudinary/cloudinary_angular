56e916c14738efa3f3ea1963e0f4019a953ff460
Eitan Peer <eitan.peer@cloudinary.com>
2017-11-23 09:00:49 +0200

    Add anuglar universal sample

---
+++ samples/angular-universal-sample/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'my-app'}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
