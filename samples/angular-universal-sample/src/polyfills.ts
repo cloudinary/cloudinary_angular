2671c37804cc6cd3c6d1636b846bf8e6958d6b10
Eitan Peer <eitan.peer@cloudinary.com>
2018-05-23 11:38:02 +0300

    Update angular-universal-sample to Angular 6

--- samples/angular-universal-sample/src/polyfills.ts
+++ samples/angular-universal-sample/src/polyfills.ts
@@ -37,17 +37,13 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
-
-
 /** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
 /**
- * Required to support Web Animations `@angular/platform-browser/animations`.
+ * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
