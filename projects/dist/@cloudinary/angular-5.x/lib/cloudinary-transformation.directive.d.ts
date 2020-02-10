0162b9ec263763db558f04d83a46243a8ffc0fcc
Raya Straus <raya@cloudinary.com>
2020-02-10 20:43:22 +0200

    deleted dist

--- projects/dist/@cloudinary/angular-5.x/lib/cloudinary-transformation.directive.d.ts
+++
@@ -1,6 +0,0 @@
-import { ElementRef } from '@angular/core';
-export declare class CloudinaryTransformationDirective {
-    private el;
-    constructor(el: ElementRef);
-    getAttributes(): NamedNodeMap;
-}
--- projects/dist/@cloudinary/angular-5.x/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/cloudinary.service';
-export * from './lib/cloudinary.module';
