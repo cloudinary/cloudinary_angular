c67c4e9731b526c7887d5fb697897f798026c822
strausr <Raya@cloudinary.com>
2020-07-19 14:02:36 +0300

    Add test coverage requirements (#285)

--- projects/angular-cld/karma.conf.js
+++ projects/angular-cld/karma.conf.js
@@ -19,7 +19,13 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
